196ee93069894faab8b8a86850d4ec1cc1eeff0d
Kyle Holtz <keholtz08@gmail.com>
2025-01-16 15:56:50 -0500

    Started  to add joining

--- Main.java
+++ Main.java
@@ -4,13 +4,21 @@ import java.util.Scanner;
 import Backend.Session;
 import gameplay.Game;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
+
         Scanner temp = new Scanner(System.in);
-        final String sessionSpacePath = "/home/kyleh/Documents/adsfg"; //"G:\\High School\\WuestC\\Drop Box\\KE_Multi_2";
+
+        final String sessionSpacePath = "G:\\High School\\WuestC\\Drop Box\\KE_Multi_2";
+        final boolean hosting = false;// DEBUGGING - change this locally to test hosting/player joining
         ArrayList<Game> games = new ArrayList<Game>();
-        Session session = new Session("A", sessionSpacePath, "Kyle", games);
-        temp.next();
+        Session session;
+        if (hosting) {
+            session = new Session("A", sessionSpacePath, "Kyle", games);
+        } else {
+            session = new Session("A", sessionSpacePath, "Kyle");
+        }
+        temp.next();//DEBUG - Wait to clean up 
         session.clean();
     }
 }
