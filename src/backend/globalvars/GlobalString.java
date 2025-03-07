2be4546a9b2ca1beae17e646b340a0d7da768969
pianogar <157652579+pianogar@users.noreply.github.com>
2025-03-07 12:15:52 -0500

    int to string

--- src/backend/globalvars/GlobalString.java
+++ src/backend/globalvars/GlobalString.java
@@ -3,15 +3,15 @@ package backend.globalvars;
 import backend.Session;
 
 public class GlobalString extends GlobalVar<String>{
-    private static final int BACKSLASH = 1;
-    private static final int SLASH = 2;
-    private static final int COLON = 3;
-    private static final int ASTERISK = 4;
-    private static final int QUESTION_MARK = 5;
-    private static final int QUOTE = 6;
-    private static final int LESS = 7;
-    private static final int GREATER = 8;
-    private static final int PIPE = 9;
+    private static final String BACKSLASH = "1";
+    private static final String SLASH = "2";
+    private static final String COLON = "3";
+    private static final String ASTERISK = "4";
+    private static final String QUESTION_MARK = "5";
+    private static final String QUOTE = "6";
+    private static final String LESS = "7";
+    private static final String GREATER = "8";
+    private static final String PIPE = "9";
     private static final String ESCAPE = "#";
     public GlobalString(Session session, String name){
         super(session, name, String::valueOf);
