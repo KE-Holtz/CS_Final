0784e6376b3f0db831e5969b116ba1e8dbe9308f
pianogar <157652579+pianogar@users.noreply.github.com>
2025-05-27 12:15:28 -0400

    Create TicTacToe.java

---
+++ src/gameplay/games/TicTacToe.java
@@ -0,0 +1,33 @@
+package gameplay.games;
+
+import backend.Session;
+
+public class TicTacToe extends Game{
+
+    public TicTacToe () {
+        setName("TicTacToe");
+    }
+    @Override
+    public void initialize(Session session) {
+        
+    }
+
+    @Override
+    public void startGame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'startGame'");
+    }
+
+    @Override
+    public boolean periodic() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'periodic'");
+    }
+
+    @Override
+    public void endGame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'endGame'");
+    }
+
+}
