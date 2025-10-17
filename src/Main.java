257df3162e5ee435c97f8a3fe47b8b556acc4884
pianogar <nathanpcrane@gmail.com>
2025-10-17 11:49:30 -0400

    playerNameFix

--- src/Main.java
+++ src/Main.java
@@ -306,7 +306,7 @@ public class Main {
                 return "Invalid character: " + c;
             }
         }
-        if (sessionSpacePath.resolve(sessionName).resolve("players").toFile().exists()) {
+        if (sessionSpacePath.resolve(sessionName).resolve("players").resolve(name).toFile().exists()) {
             return "Name already exists.";
         }
         return "";
